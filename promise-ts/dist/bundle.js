c54df6515bc0fb4784a7ece95c2f7db66b802eee
lyf <lin13177896353@163.com>
2022-09-06 11:19:16 +0800

    修改提交

--- promise-ts/dist/bundle.js
+++ promise-ts/dist/bundle.js
@@ -1,4 +1,17 @@
 'use strict';
 
-let str = '1111';
-console.log(str);
+// promise 都是基于回调模式
+// }
+Function.prototype.before = function (fn) {
+    return (...args) => {
+        fn();
+        this(...args); //调用原有的core方法
+    };
+};
+function core(...args) {
+    console.log(...args);
+}
+let fn = core.before(() => {
+    console.log('core.before');
+});
+fn(1, 2, 3);
--- promise-ts/src/index.ts
+++ promise-ts/src/index.ts
@@ -1,2 +1,24 @@
-let str:string = '1111'
-console.log(str)
+// promise 都是基于回调模式
+
+// 高阶函数 1如果你的函数参数是一个函数 2你的函数的返回的是一个函数
+
+type Callback = ()=>void
+type ReturnFn = (...args:any[])=>void
+// declare global {
+    interface Function{ //接口的合并
+        before(fn:Callback):ReturnFn
+    }
+// }
+Function.prototype.before = function(fn){
+    return (...args)=>{
+        fn();
+        this(...args); //调用原有的core方法
+    }
+}
+function core(...args){
+    console.log(...args)
+}
+let fn = core.before(()=>{
+    console.log('core.before')
+})
+fn(1,2,3);
